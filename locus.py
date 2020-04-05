0b689f3dee41c39d8fc154532f9d6e2909226421
matt <61066167+cola99@users.noreply.github.com>
2020-04-05 13:56:01 +0100

    Update locus.py

--- locus.py
+++ locus.py
@@ -12,7 +12,7 @@ for x in data:
     data = gpsphoto.getGPSData(x)
     all_coords.append([data.get("Latitude"), data.get("Longitude")]) # Get only the lat,long keys.
 
-# Build map with folium and map coordinates from the list.
+# Build a map with folium and map coordinates from the list.
 m = folium.Map(location=[51.501622222222224, -0.1407861111111111], zoom_start=5) # Default location is a view of United Kingdom.
 for coord in all_coords: 
 	folium.Marker(location=coord, icon=folium.Icon(color='red')).add_to(m)
